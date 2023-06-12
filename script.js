07e31f4ad1616f728ee940cca6b56cc023065d45
Yota Okabe <93124377+Yota-Okabe@users.noreply.github.com>
2023-06-12 15:28:55 +0900

    .js カウントダウン機能

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ function real_time(){
     elem.innerHTML = year+"年"+month+"月"+day+"日"+" "+hours+"時"+minutes+"分"+seconds+"秒" ;
 }
 setInterval("real_time()", 1000);
+
+
+// 機能①『カウントダウン』
+const hour = document.getElementById("hour");
+const min = document.getElementById("min");
+const sec = document.getElementById("sec");
+
+function countdown() {
+    const now = new Date();
+    const tomorrow = new Date(now.getFullYear(),now.getMonth(),now.getDate()+1);
+    const diff = tomorrow.getTime() - now.getTime();
+    const calcHour = Math.floor(diff / 1000 / 60 / 60) -6;
+    const calcMin = Math.floor(diff / 1000 / 60) % 60;
+    const calcSec = Math.floor(diff / 1000) % 60;
+
+    hour.innerHTML = calcHour < 10 ? '0' + calcHour : calcHour;
+    min.innerHTML = calcMin < 10 ? '0' + calcMin : calcMin;
+    sec.innerHTML = calcSec < 10 ? '0' + calcSec : calcSec;
+}
+setInterval("countdown()", 1000);
+
